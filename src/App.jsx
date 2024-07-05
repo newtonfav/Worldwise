1241e17c0aa11e5981fc93a59f3e687decdbc464
Favour Oghenekowho <gh.oghene@gmail.com>
2024-07-05 20:03:27 +0100

    added params to url

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountriesList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:2500";
 
@@ -48,6 +49,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountriesList isLoading={isLoading} cities={cities} />}
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <div>
+        position: {lat}, {lng}
+      </div>
+    </div>
+  );
 }
 
 export default Map;
